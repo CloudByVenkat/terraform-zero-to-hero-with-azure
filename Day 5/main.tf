50900df3e0f9732edec42e43e3d3bb7aa8e6bbe6
Subbareddy Vundela (Venkat) <CloudByVenkat@gmail.com>
2026-01-29 13:06:32 +0000

    Add Terraform configuration for Azure resources

---
+++ Day 5/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.58.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-terraform-demo"
+  location = "eastus"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "tfdevstorage01"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
