371a7271fb5521e8eb6f4ed8c3699b08c9e1dc8a
Subbareddy Vundela (Venkat) <CloudByVenkat@gmail.com>
2026-02-20 10:29:18 +0000

    Add files via upload

---
+++ day-17-keyvault-security/locals.tf
@@ -0,0 +1,10 @@
+locals {
+  common_tags = {
+    Environment = var.environment
+    Project     = var.project_name
+    ManagedBy   = "Terraform"
+    Owner       = "Solutions Architect"
+    Day         = "17"
+    Security    = "KeyVault-Enabled"
+  }
+}
